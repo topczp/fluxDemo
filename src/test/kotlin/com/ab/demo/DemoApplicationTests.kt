7e08d88477bb2f40d7c88d04b2e5703a360566c6
Andrzej Biernacki <andrzej.biernacki@accenture.com>
2021-09-09 09:15:08 +0200

    tests with overriding parameters

--- src/test/kotlin/com/ab/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/ab/demo/DemoApplicationTests.kt
@@ -1,13 +1,34 @@
 package com.ab.demo
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
+
+    @Autowired
+    @Value("\${envParam}")
+    lateinit var propertyFromConfig: String
+
+//    @Autowired
+//    @Value("\${spring.profiles.active}")
+//    lateinit var activeProfile: String
+
+    @Autowired
+    lateinit var environment: Environment
 
     @Test
     fun contextLoads() {
+        assertThat(propertyFromConfig).isEqualTo("overridden in test")
+        val activeProfileFromEnvironment = environment.activeProfiles[0]
+        assertThat(activeProfileFromEnvironment).isEqualTo("test")
+//        assertThat(activeProfile).isEqualTo("from application properties")
     }
 
 }
